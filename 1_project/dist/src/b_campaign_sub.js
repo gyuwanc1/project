9b79d7a26aa25f50e3d1b342fbaf17c4970ec24b
b_gyuwan <gyuwanc@gmail.com>
2021-12-29 20:59:29 +0900

    [modified] 1_project JS add

---
+++ 1_project/dist/src/b_campaign_sub.js
@@ -0,0 +1,18 @@
+//b_campaign_sub.ks
+
+const headBox = document.querySelector('#headBox');
+const menuWrap = headBox.querySelector('.menu_wrap');
+const mobilBtn = menuWrap.querySelector('.mobile_gnb_btn');
+const modalmenu =  headBox.querySelector('.mobile_menu');
+
+const closeBtn = modalmenu.querySelector('.close_btn');
+
+mobilBtn.addEventListener('click', e =>{
+  e.preventDefault();  
+  modalmenu.classList.add('on')
+});
+
+closeBtn.addEventListener('click', e=> {
+  e.preventDefault();
+  modalmenu.classList.remove('on');
+})
