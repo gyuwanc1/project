aa382db2f0e19f4b68da2b76c8c38684bc2e6711
b_gyuwan <gyuwanc@gmail.com>
2021-12-29 16:38:32 +0900

    [modified] click test

--- 1_project/test/mob_click.js
+++ 1_project/test/mob_click.js
@@ -3,7 +3,7 @@
 const slide_box = document.querySelector('.slide_box');
 const next = slide_box.querySelector('.next');
 
-next.addEventListener('mouseenter', (e)=>{
+next.addEventListener('click', (e)=>{
   e.preventDefault();
   alert('apple test!!');
 })
