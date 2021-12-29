9d32220a6ab88fa7be6d1e05b86e58f52c0ffb10
b_gyuwan <gyuwanc@gmail.com>
2021-12-29 16:57:51 +0900

    [modified] ios

--- 1_project/test/mob_click.js
+++ 1_project/test/mob_click.js
@@ -10,14 +10,22 @@ const slideLen = viewList.length;
 
 let slideIndex = 0;
 const SLIDE_WIDTH = slideBox.clientWidth;
+viewBox.style.width = 100 * (slideLen + 1) +'%';
+viewList.forEach(el=>{
+  el.style.width = (100 / (slideLen + 1) ) + '%';
+})
 
+const cloneLI = viewList.at(0).cloneNode(true);
+viewBox.append(cloneLI);
+
+viewBox.style.transition = 'margin-left 300ms ease';
 
 next.addEventListener('click', (e)=>{
   e.preventDefault();
   if(slideIndex < slideLen-1) {
     slideIndex += 1; 
   }else{ 
-    slideIndex = 0;
+    slideIndex = 0;    
   }
   const moveWidth = slideIndex *  SLIDE_WIDTH;
   viewBox.style.marginLeft = moveWidth * -1 + 'px';
@@ -25,3 +33,4 @@ next.addEventListener('click', (e)=>{
 });
 
 }
+//되요 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ미친다 
