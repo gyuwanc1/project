2368d1d51e62fb73d2be1ff02bdefc0306e6f793
b_gyuwan <gyuwanc@gmail.com>
2021-12-29 16:41:17 +0900

    [modified] ..test

--- 1_project/test/mob_click.js
+++ 1_project/test/mob_click.js
@@ -1,5 +1,6 @@
 
 
+{
 const slide_box = document.querySelector('.slide_box');
 const next = slide_box.querySelector('.next');
 
@@ -7,3 +8,12 @@ next.addEventListener('click', (e)=>{
   e.preventDefault();
   alert('apple test!!');
 })
+}
+//잠시만 기다려주세요  바로 안되더라구요
+
+//클릭도되요
+
+//버튼 화면에 나오는건 잡았는데
+
+//클릭하면 슬라이드가안넘어가요 
+//넵 
