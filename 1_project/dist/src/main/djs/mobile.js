98aaea9597f9500af93850680ad19631b5b68513
b_gyuwan <gyuwanc@gmail.com>
2022-01-04 22:06:26 +0900

    [modified] smooth

--- 1_project/dist/src/main/djs/mobile.js
+++ 1_project/dist/src/main/djs/mobile.js
@@ -4,6 +4,9 @@ const headBox = document.querySelector('#headBox');
 const menuWrap = headBox.querySelector('.menu_wrap');
 const mobilBtn = menuWrap.querySelector('.mobile_gnb_btn');
 const modalmenu =  menuWrap.querySelector('.mobile_menu');
+// const elUl = modalmenu.querySelector('.elUl')
+// const btn = elUl.querySelector('.cam_story')
+// const rBtn = btn.querySelector('a')
 
 const closeBtn = modalmenu.querySelector('.close_btn');
 
@@ -17,4 +20,10 @@ closeBtn.addEventListener('click', e=> {
   modalmenu.classList.remove('on');
 })
 
+
+// rBtn.addEventListener('click',(e) => {
+//   e.preventDefault;
+//   const intro = document.querySelector('#intro').offsetTop;    
+//   window.scrollTo({top:intro, behavior:'smooth'});
+// })
 }
