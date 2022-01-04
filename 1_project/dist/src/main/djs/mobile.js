d1ea0c033c250d35911369b4e4943fff54dedf13
b_gyuwan <gyuwanc@gmail.com>
2022-01-04 23:53:56 +0900

    [modified] scroll to Js source code && sub_page Image change

--- 1_project/dist/src/main/djs/mobile.js
+++ 1_project/dist/src/main/djs/mobile.js
@@ -4,9 +4,13 @@ const headBox = document.querySelector('#headBox');
 const menuWrap = headBox.querySelector('.menu_wrap');
 const mobilBtn = menuWrap.querySelector('.mobile_gnb_btn');
 const modalmenu =  menuWrap.querySelector('.mobile_menu');
-// const elUl = modalmenu.querySelector('.elUl')
-// const btn = elUl.querySelector('.cam_story')
-// const rBtn = btn.querySelector('a')
+const elUl = modalmenu.querySelector('.elUl')
+const btn01 = elUl.querySelector('.cam_story')
+const btn02 = elUl.querySelector('.pro_story')
+const btn03 = elUl.querySelector('.admin_advice')
+
+
+
 
 const closeBtn = modalmenu.querySelector('.close_btn');
 
@@ -17,13 +21,26 @@ mobilBtn.addEventListener('click', e =>{
 
 closeBtn.addEventListener('click', e=> {
   e.preventDefault();
-  modalmenu.classList.remove('on');
+  modalmenu.classList.remove('on');  
 })
 
 
-// rBtn.addEventListener('click',(e) => {
-//   e.preventDefault;
-//   const intro = document.querySelector('#intro').offsetTop;    
-//   window.scrollTo({top:intro, behavior:'smooth'});
-// })
+btn01.addEventListener('click',(e) => {      
+  e.preventDefault(); 
+  const intro = document.querySelector('#intro').offsetTop;  
+  window.scrollTo({top:intro, behavior: 'smooth'})  
+})
+
+btn02.addEventListener('click',(e) => {
+  e.preventDefault();       
+  const intro = document.querySelector('#ex_booth').offsetTop;    
+  window.scrollTo({top:intro, behavior: 'smooth'})  
+})
+
+btn03.addEventListener('click',(e) => {
+  e.preventDefault();       
+  const intro = document.querySelector('#advice_area').offsetTop;   
+  window.scrollTo({top:intro, behavior: 'smooth'})  
+})
+
 }
--- 1_project/js/src/b_campaign_sub.js
+++ 1_project/js/src/b_campaign_sub.js
@@ -4,6 +4,9 @@ const headBox = document.querySelector('#headBox');
 const menuWrap = headBox.querySelector('.menu_wrap');
 const mobilBtn = menuWrap.querySelector('.mobile_gnb_btn');
 const modalmenu =  headBox.querySelector('.mobile_menu');
+const elUl = modalmenu.querySelector('.elUl')
+const btn01 = elUl.querySelector('.into_pro')
+const btn02 = elUl.querySelector('.watch_movie')
 
 const closeBtn = modalmenu.querySelector('.close_btn');
 
@@ -15,5 +18,17 @@ mobilBtn.addEventListener('click', e =>{
 closeBtn.addEventListener('click', e=> {
   e.preventDefault();
   modalmenu.classList.remove('on');
+})
+
+btn01.addEventListener('click',(e) => {      
+  e.preventDefault(); 
+  const intro = document.querySelector('#program_int').offsetTop;  
+  window.scrollTo({top:intro, behavior: 'smooth'})  
+})
+
+btn02.addEventListener('click',(e) => {
+  e.preventDefault();       
+  const intro = document.querySelector('#program_int').offsetTop;    
+  window.scrollTo({top:intro, behavior: 'smooth'})  
 })
 }
