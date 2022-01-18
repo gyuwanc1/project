b32e18fa59775afc2c6c76167645c96803381dd0
b_gyuwan <gyuwanc@gmail.com>
2022-01-18 11:47:47 +0900

    [base completed] personal page

--- project_personalweb/src/component/Portfolio.js
+++ project_personalweb/src/component/Portfolio.js
@@ -4,7 +4,31 @@ import '../style/Portfolio.scss'
 export default function Portfolio() {
   return (
     <section id="portarea">
-      asdasd
+      <ul className='port_box'>
+        <li className='card'>
+          <div className='img_box01'></div>
+          <dl>
+            <dt>navienhouse</dt>
+            <dd>ui/ux design</dd>
+            <dd>use lang: html scss</dd>
+          </dl>
+          <div className='link_addr'>
+            <a href="https://bit.ly/gyuwanc02" target='_blank'>detail site</a>
+          </div>
+        </li>
+        <li className='card'>
+          <div className='img_box02'></div>
+          <dl>
+            <dt>fursys</dt>
+            <dd>clone coding</dd>
+            <dd>use lang: html scss js</dd>
+          </dl>
+          <div className='link_addr'>
+            <a href="https://bit.ly/gyuwanc1" target='_blank'>detail site</a>
+          </div>
+        </li>
+
+      </ul>
     </section>
   )
 }
